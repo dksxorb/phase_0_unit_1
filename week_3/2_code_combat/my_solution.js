5bd4b0829d2ddc9a1ccff518a50de46a39b6c209
eddiekimosabe <eddiekimosabe@gmail.com>
2014-05-28 18:18:50 -0400

    Update my_solution.js

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,4 +1,4 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself]
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
@@ -7,10 +7,82 @@
  
  
  
+ //Rescue Mission
+Move Down (x2)
+Move Right (x1)
+Move Up (x2)
+Move Right (x2)
+Move Down(x1)
+Attack Nearby Enemy
  
- 
- 
- 
+//Grab the Mushroom
+Move Up(x1)
+Move Right(x1)
+Move Left(x1)
+Move Up(x1)
+Attack Nearby Enemy
+
+//Drink Me
+Move Right(x1)
+Attack Nearby Enemy
+Move Right(x1)
+Move Down(x1)
+Move Up(x1)
+Move Right(x1)
+Attack Nearby Enemy
+
+//Taunt the Guards
+Move Right(x1)
+Bust down door
+Move Right(x1)
+Say "Hey There!"
+Move Left(x2)
+Say "Attack!"
+Move Right(x1)
+Say "Follow me"
+Move Right(x2)
+Move Up(x1)
+Move Right(x1)
+Move Down(x1)
+Move Right(x1)
+Say "Hey There!"
+Move Left(x1)
+Say "Attack!"(x2)
+Say "Follow me."
+Move Right(x1)
+Move Up(x1)
+Move Right(x1)
+
+//It's a trap
+Move down(x2)
+Say "Hey There"
+move Up (x2)
+Say "attack!"
+
+//Taunt
+Say "Hey"
+Say "Yo"
+Say "Idiot"
+Say "Yeah"
+
+//Cowardly Taunt
+MoveXY (51,37)
+Say "Hey Dumbos!"
+MoveXY (70,10)
+
+//Commanding Followers
+Say "Follow Me!"
+Move XY	(62,41)
+Say "Attack!"
+
+//Mobile artillery
+moveXY (30,26)
+AttackXY(46,5)
+moveXY(46,40)
+AttackXY(69,59)
+Attack(61,47)
+Attack(49,67)
+Attack(46,52)
  
 // Reflection:
 // Write your reflection here.
