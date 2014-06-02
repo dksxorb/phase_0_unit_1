89b0d1ee32ad4131d7ce6ce4302a1d22c7e644cb
eddiekimosabe <eddiekimosabe@gmail.com>
2014-06-01 21:22:27 -0400

    Update my_solution.js

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -1,16 +1,31 @@
-// I worked [by myself, with:] on this challenge
+// I worked [by myself] on this challenge
 
 // Your mission description:
-// 
+// You(Tharin) find yourself captured by the ogre horde and thrown in one of their prison cells. The majority of the ogres all 
+// left to do battle with the rest of your combat mates and only one unhappy ogre guard is there to stand watch.
+// There appears to be an iron pipe in the right corner of the cell. Silly ogres didn't even notice it when they threw
+// you in there. You're very aware of the fact that ogres are particularly sensitive creatures especially when it comes
+// to their mothers. Taunt the ogre and talk about his mother. Taunt him enough so that he opens the door to the cell
+// in a fit of rage to beat you up. Take that chance to knock him unconscious with the iron pipe and escape from the dungeon.
 // 
 // 
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// Make object Tharin
+// Make object ogre
+// Make object pipe
+
+// Move Tharin to pipe location
+// Tharin picks up pipe when he steps near the pipe
+// Move Tharin close to cell door
+// Taunt Ogre with any string that contains Mother
+// Ogre says don't you talk about my mother
+// Ogre moves to door
+// Ogre opens door
+// Tharin hits ogre once
+// Ogre goes unconscious
+// Move Tharin Down
+// Move Tharin Right to the exit
 
 // Initial Code
 
